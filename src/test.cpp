c8227e56eedc5429ff7bbdf22e6c7404dd386e79
Alexander Strack <strack@MacBookPro-Al.fritz.box>
2022-07-22 18:31:26 +0200

    try boost

--- src/test.cpp
+++ src/test.cpp
@@ -6,56 +6,70 @@
 #include <hpx/local/init.hpp>
 #include <hpx/modules/format.hpp>
 
+#include <boost/version.hpp>
+#include <boost/numeric/ublas/vector.hpp>
+//#include <boost/numeric/ublas.hpp>
+
+namespace ublas = boost::numeric::ublas;
+
+std::vector<CALC_TYPE> gen_tile(std::size_t row, std::size_t col, std::size_t tile_size, std::size_t n_tiles)
+{
+   std::size_t i_global,j_global;
+   // Initialize tile
+   std::vector<CALC_TYPE> tile;
+   tile.resize(tile_size * tile_size);
+
+   for(std::size_t i = 0; i < tile_size; i++)
+   {
+      i_global = (n_tiles - 1) * tile_size + i;
+      for(std::size_t j = 0; j < tile_size; j++)
+      {
+         j_global = (n_tiles - 1) * tile_size + j;
+         tile[i * tile_size + j] = 1; //covariance_function(i_global,j_global);
+      }
+   }
+   return tile;
+}
+
 int hpx_main(hpx::program_options::variables_map& vm)
 {
-    //std::size_t N = vm["N"].as<std::size_t>();
-    //std::size_t T = vm["T"].as<std::size_t>();
-    //std::string out = vm["out"].as<std::string>();
+  std::size_t n_train = vm["n_train"].as<std::size_t>();  //max 100*1000
+  std::size_t n_test = vm["n_test"].as<std::size_t>();     //max 5*1000
+  std::size_t n_regressors = vm["n_regressors"].as<std::size_t>();
+  std::size_t n_tiles = vm["n_tiles"].as<std::size_t>();
 
-    hpx::chrono::high_resolution_timer t;
-    /*
-    std::vector<hpx::shared_future<std::vector<CALC_TYPE>>> A_tiles;
-    A_tiles.resize(T * T);
+  std::size_t tile_size = n_train / n_tiles;
 
-    for (std::size_t i = 0; i < T; ++i)
-    {
-       for (std::size_t j = 0; j < T; ++j)
-       {
-          A_tiles[i * T + j] = hpx::dataflow(&gen_tile, i, j, N, T);
-       }
-    }
+  hpx::chrono::high_resolution_timer t;
 
-    if (out == "debug")
-    {
-       for (std::size_t i = 0; i < T; ++i)
-       {
-          for (std::size_t j = 0; j < T; ++j)
-          {
-             print_tile(A_tiles[i * T + j].get(), "a", i, j, N);
-          }
-       }
-    }
 
-    lu_tiled(A_tiles, N, T);
+  ublas::vector<CALC_TYPE> vec;
+  std::cout << "Using Boost "
+          << BOOST_VERSION / 100000     << "."  // major version
+          << BOOST_VERSION / 100 % 1000 << "."  // minor version
+          << BOOST_VERSION % 100                // patch level
+          << std::endl;
 
-    if (out == "debug")
-    {
-       for (std::size_t i = 0; i < T; ++i)
-       {
-          for (std::size_t j = 0; j < T; ++j)
-          {
-             print_tile(A_tiles[i * T + j].get(), "lu", i, j, N);
-          }
-       }
-    }
-    */
-    double elapsed = t.elapsed();
-    std::cout << "Elapsed " << elapsed << " s\n";
-    return hpx::local::finalize();    // Handles HPX shutdown
+  std::vector<hpx::shared_future<std::vector<CALC_TYPE>>> K_tiles;
+  K_tiles.resize(n_tiles * n_tiles);
+  // Assemble K
+  for (std::size_t i = 0; i < n_tiles; i++)
+  {
+     for (std::size_t j = 0; j < n_tiles; ++j)
+     {
+        //K_tiles[i * n_tiles + j] = hpx::dataflow(&gen_tile, i, j, tile_size, n_tiles);
+     }
+  }
+  // Compute Cholesky decomposition
+  //left_looking_cholesky(K_tiles,tile_size, n_tiles)
+
+  double elapsed = t.elapsed();
+  std::cout << "Elapsed " << elapsed << " s\n";
+  return hpx::local::finalize();    // Handles HPX shutdown
 }
 
 int main(int argc, char* argv[])
-  {
+{
     // hpx
     hpx::program_options::options_description desc_commandline;
     hpx::local::init_params init_args;
@@ -68,13 +82,14 @@ int main(int argc, char* argv[])
          "Number of test samples (max 5 000)")
         ("n_regressors", hpx::program_options::value<std::size_t>()->default_value(100),
         "Number of delayed input regressors")
+        ("n_tiles", hpx::program_options::value<std::size_t>()->default_value(10),
+        "Number of tiles per dimension -> n_tiles * n_tiles total")
     ;
     hpx::program_options::variables_map vm;
     hpx::program_options::store(hpx::program_options::parse_command_line(argc, argv, desc_commandline), vm);
     // GP parameters
-    int       n_train = vm["n_train"].as<std::size_t>();  //max 100*1000
-    int       n_test = vm["n_test"].as<std::size_t>();     //max 5*1000
-    int       n_regressors = vm["n_regressors"].as<std::size_t>();
+    std::size_t       n_train = vm["n_train"].as<std::size_t>();  //max 100*1000
+    std::size_t       n_test = vm["n_test"].as<std::size_t>();     //max 5*1000
     CALC_TYPE    hyperparameters[3];
     // initalize hyperparameters to empirical moments of the data
     hyperparameters[0] = 1.0;   // lengthscale = variance of training_output
@@ -120,7 +135,6 @@ int main(int argc, char* argv[])
     fclose(training_output_file);
     fclose(test_input_file);
     fclose(test_output_file);
-    printf("train %d, test %d, reg %d", n_train, n_test, n_regressors);
     ////////////////////////////////////////////////////////////////////////////
     // Run HPX
     init_args.desc_cmdline = desc_commandline;
