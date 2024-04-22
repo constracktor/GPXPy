fd59506e49f030dec12841b3a9f255d317d453e0
MHelmann <maksim.helmann@gmail.com>
2024-04-22 12:57:23 +0200

    [uncertainty] implement mkl_trsm_matrix and mkl_gemm_matrix

---
+++ src/headers/uncertainty.hpp
@@ -0,0 +1,39 @@
+//#include "mkl.h
+#include "mkl_cblas.h"
+#include "mkl_lapacke.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// BLAS operations for tiled cholkesy
+// in-place solve L * X = A^T where L triangular
+template <typename T>
+std::vector<T> mkl_trsm_matrix(std::vector<T> L,
+                        std::vector<T> A,
+                        std::size_t N,
+                        std::size_t M)
+{
+  // TRSM constants
+  const T alpha = 1.0f; 
+  // TRSM kernel - caution with dtrsm
+  cblas_strsm(CblasRowMajor, CblasLeft, CblasLower, CblasNoTrans, CblasNonUnit, N, M, alpha, L.data(), N, A.data(), M);
+  // return vector
+  return A;
+}
+
+
+//C = C - A * B
+template <typename T>
+std::vector<T> mkl_gemm_matrix(std::vector<T> A,
+                        std::vector<T> B,
+                        std::vector<T> C,
+                        std::size_t N,
+                        std::size_t M)
+{
+  // GEMM constants
+  const T alpha = -1.0f;
+  const T beta = 1.0f;
+  // GEMM kernel - caution with dgemm
+  cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+              N, M, N, alpha, A.data(), N, B.data(), M, beta, C.data(), M);
+  // return vector
+  return C;
+}
