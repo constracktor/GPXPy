33eb398f0d689803d0dbc3ddc76455b936cf286f
MHelmann <maksim.helmann@gmail.com>
2024-04-22 17:39:19 +0200

    [uncertainty] implement mkl_trsm_u_matrix and mkl_gemm_u_matrix

--- src/headers/uncertainty.hpp
+++ src/headers/uncertainty.hpp
@@ -4,9 +4,9 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 // BLAS operations for tiled cholkesy
-// in-place solve L * X = A^T where L triangular
+// in-place solve L * X = A where L lower triangular
 template <typename T>
-std::vector<T> mkl_trsm_matrix(std::vector<T> L,
+std::vector<T> mkl_trsm_l_matrix(std::vector<T> L,
                         std::vector<T> A,
                         std::size_t N,
                         std::size_t M)
@@ -22,7 +22,7 @@ std::vector<T> mkl_trsm_matrix(std::vector<T> L,
 
 //C = C - A * B
 template <typename T>
-std::vector<T> mkl_gemm_matrix(std::vector<T> A,
+std::vector<T> mkl_gemm_l_matrix(std::vector<T> A,
                         std::vector<T> B,
                         std::vector<T> C,
                         std::size_t N,
@@ -33,6 +33,40 @@ std::vector<T> mkl_gemm_matrix(std::vector<T> A,
   const T beta = 1.0f;
   // GEMM kernel - caution with dgemm
   cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+              N, M, N, alpha, A.data(), N, B.data(), M, beta, C.data(), M);
+  // return vector
+  return C;
+}
+
+// in-place solve L^T * X = A where L upper triangular
+template <typename T>
+std::vector<T> mkl_trsm_u_matrix(std::vector<T> L,
+                        std::vector<T> A,
+                        std::size_t N,
+                        std::size_t M)
+{
+  // TRSM constants
+  const T alpha = 1.0f; 
+  // TRSM kernel - caution with dtrsm
+  cblas_strsm(CblasRowMajor, CblasLeft, CblasUpper, CblasTrans, CblasNonUnit, N, M, alpha, L.data(), N, A.data(), M);
+  // return vector
+  return A;
+}
+
+
+//C = C - A^T * B
+template <typename T>
+std::vector<T> mkl_gemm_u_matrix(std::vector<T> A,
+                        std::vector<T> B,
+                        std::vector<T> C,
+                        std::size_t N,
+                        std::size_t M)
+{
+  // GEMM constants
+  const T alpha = -1.0f;
+  const T beta = 1.0f;
+  // GEMM kernel - caution with dgemm
+  cblas_sgemm(CblasRowMajor, CblasTrans, CblasNoTrans,
               N, M, N, alpha, A.data(), N, B.data(), M, beta, C.data(), M);
   // return vector
   return C;
