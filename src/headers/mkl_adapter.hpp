027b46f1c2645aa34cb3d8a966a47bc51cb30753
MHelmann <maksim.helmann@gmail.com>
2024-05-12 23:35:29 +0200

    [mkl_adapter] implement mkl_gemm_diag

--- src/headers/mkl_adapter.hpp
+++ src/headers/mkl_adapter.hpp
@@ -71,9 +71,26 @@ std::vector<T> mkl_sger(std::vector<T> A,
                         std::size_t N)
 {
   // GER constants
-  const T alpha = 1.0f;
+  const T alpha = -1.0f;
   // GER kernel
   cblas_sger(CblasRowMajor, N, N, alpha, x.data(), 1, y.data(), 1, A.data(), N);
   // return A
   return A;
+}
+
+// C = C - A * B^T
+template <typename T>
+std::vector<T> mkl_gemm_diag(std::vector<T> A,
+                             std::vector<T> B,
+                             std::vector<T> C,
+                             std::size_t N)
+{
+  // GEMM constants
+  const T alpha = 1.0f;
+  const T beta = 1.0f;
+  // GEMM kernel
+  cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+              N, N, N, alpha, A.data(), N, B.data(), N, beta, C.data(), N);
+  // return vector
+  return C;
 }
