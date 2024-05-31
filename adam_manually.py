9a9a59e2891647765a4f52b9eb664483804944e0
MHelmann <maksim.helmann@gmail.com>
2024-05-31 11:41:51 +0200

    [adam_manually] parameter update for unconstrained variables

--- adam_manually.py
+++ adam_manually.py
@@ -4,6 +4,26 @@ import numpy as np
 from sklearn.metrics.pairwise import rbf_kernel
 from scipy.linalg import  cholesky, solve_triangular
 
+def softplus(x):
+    return np.log(1+np.exp(x))
+
+def der_sofplus(x):
+    return 1/(np.exp(-x)+1)
+
+def inv_softplus(x):
+    return np.log(np.exp(x)-1)
+
+def der_inv_sofplus(x):
+    return np.exp(x)/(np.exp(x)-1)
+
+def sigmoid(x):
+    return 1. / (1.+ np.exp(-x))
+
+def inv_sigmoid(x):
+    return np.log(x / (1-x))
+
+def der_sigmoid(x):
+    return sigmoid(x)*(1. - sigmoid(x))
 
 def generate_regressor(x_original, n_regressors):
     """Pad X with zeros from left for #regressors-1 for each data point
@@ -22,7 +42,7 @@ def generate_regressor(x_original, n_regressors):
         x_padded = np.roll(x_padded, -1)
         X.append(x_padded[:n_regressors])
 
-    X = np.array(X, dtype='f')
+    X = np.array(X, dtype='d')
 
     return X
 
@@ -42,14 +62,14 @@ def load_data(train_in_path, train_out_path, test_in_path, test_out_path, size_t
         _type_: _description_
     """
 
-    x_train_in = np.loadtxt(train_in_path, dtype='f')[:size_train]
-    x_test_in = np.loadtxt(test_in_path, dtype='f')[:size_test]
+    x_train_in = np.loadtxt(train_in_path, dtype='d')[:size_train]
+    x_test_in = np.loadtxt(test_in_path, dtype='d')[:size_test]
 
     X_train = generate_regressor(x_train_in, n_regressors)
     X_test = generate_regressor(x_test_in, n_regressors)
 
-    Y_train = np.loadtxt(train_out_path, dtype='f')[:size_train, None]
-    Y_test = np.loadtxt(test_out_path, dtype='f')[:size_test, None]
+    Y_train = np.loadtxt(train_out_path, dtype='d')[:size_train, None]
+    Y_test = np.loadtxt(test_out_path, dtype='d')[:size_test, None]
 
     return X_train, Y_train, X_test, Y_test
 
@@ -66,7 +86,7 @@ def to_unconstrained(parameter):
     # return log(exp(parameter) - 1.0);
 
 def rhs(X_train, Y_train, hyperparam):
-    K = 1.0*rbf_kernel(X_train, gamma=0.5*hyperparam) + 0.1*np.eye(X_train.shape[0])
+    K = 1.0*rbf_kernel(X_train, gamma=1./(2*hyperparam**2)) + 0.1*np.eye(X_train.shape[0])
     # print(np.all(np.linalg.eigvals(K) > 0))
     # print(np.linalg.cond(K)
     L = cholesky(K, lower=True)
@@ -77,7 +97,7 @@ def rhs(X_train, Y_train, hyperparam):
 # objective function
 def objective(X_train, Y_train, L, alpha):
     N = X_train.shape[0]
-    loss =  np.sum(np.log(np.diag(L)**2, dtype='f')) + Y_train.T.dot(alpha) + N * np.log(2*np.pi, dtype='f')
+    loss =  np.sum(np.log(np.diag(L)**2, dtype='d')) + Y_train.T.dot(alpha) + N * np.log(2*np.pi, dtype='d')
     return 0.5*loss/N
 
 def compute_distance_matrix(X, Y):
@@ -87,11 +107,11 @@ def compute_distance_matrix(X, Y):
 # derivative of objective function
 def derivative(X_train, Y_train, K, alpha, hyperparam):
     N = X_train.shape[0]
-    # grad_noise = np.eye(X_train.shape[0], X_train.shape[0]) * np.sqrt(hyperparam) * 2
+    # grad_noise = np.eye(X_train.shape[0], X_train.shape[0]) * sigmoid(inv_softplus(0.1 - 1.e-6))
     # return 0.5 * 1/N * np.trace( np.linalg.solve(K, (np.eye(X_train.shape[0]) - np.outer(Y_train, alpha))) @  grad_noise )
-    # grad_v = 2.0 * np.sqrt(hyperparam) * rbf_kernel(X_train, gamma=0.5)
+    # grad_v = rbf_kernel(X_train, gamma=0.5) * der_sofplus(inv_softplus(hyperparam))
     # return 0.5 * 1/N * np.trace( np.linalg.solve(K, (np.eye(X_train.shape[0]) - np.outer(Y_train, alpha))) @  grad_v )
-    grad_l = 1.0 / (sqrt(hyperparam) * hyperparam) * rbf_kernel(X_train, gamma=0.5*hyperparam) * compute_distance_matrix(X_train, X_train)
+    grad_l = 1.0/ (hyperparam**3) * compute_distance_matrix(X_train, X_train) * rbf_kernel(X_train, gamma=1./(2*(hyperparam**2))) * sigmoid(inv_softplus(hyperparam))
     return 0.5 * 1/N * np.trace( np.linalg.solve(K, (np.eye(X_train.shape[0]) - np.outer(Y_train, alpha))) @  grad_l  )
 
 # gradient descent algorithm with adam
@@ -106,7 +126,7 @@ def adam(objective, derivative, X_train, Y_train, noise, n_iter, alpha, beta1, b
     # run the gradient descent updates
     for t in range(n_iter):
         
-        print(f'>{t} f({noise:.7f}) = {score[0][0]:.6f}')
+        print(f'>{t} f({noise:.10f}) = {score[0][0]:.10f}')
         # calculate gradient g(t)
         g = derivative(X_train, Y_train, K, a, noise)
         # build a solution one variable at a time
@@ -146,7 +166,7 @@ if __name__ == "__main__":
     test_in_file = "src/data/test/test_input.txt"
     test_out_file = "src/data/test/test_output.txt"
 
-    size_train = 10
+    size_train = 100
     size_test = 10
     n_regressors = 100
 
@@ -160,9 +180,9 @@ if __name__ == "__main__":
         n_regressors,
     )
     
-    noise = 1.0
+    noise = 0.5
     # number of optimisation steps
-    n_iter = 11
+    n_iter = 2
     # steps size
     alpha = 0.001
     # factor for average gradient
@@ -172,4 +192,4 @@ if __name__ == "__main__":
     # perform the gradient descent search with adam
     best, score = adam(objective, derivative, X_train, Y_train, noise, n_iter, alpha, beta1, beta2)
     print('Done!')
-    print(f'f({best:.6f}) = {score:.6f}')
+    print(f'f({best:.10f}) = {score:.10f}')
