d7357a77993427845a07dcbd0b7c1a545d17b1e9
MHelmann <maksim.helmann@gmail.com>
2024-06-04 16:14:54 +0200

    [uncertainty] implement mkl_gemm_u_KK, mkl_gemm_u_KK, mkl_gemm_u_KK, mkl_trsm_l_KK

--- src/headers/uncertainty.hpp
+++ src/headers/uncertainty.hpp
@@ -101,3 +101,65 @@ std::vector<double> diag(const std::vector<double> &A,
 
   return std::move(tile);
 }
+
+// in-place solve X * L = A where L lower triangular
+std::vector<double> mkl_trsm_l_KK(std::vector<double> L,
+                                  std::vector<double> A,
+                                  std::size_t N,
+                                  std::size_t M)
+{
+  // TRSM constants
+  const double alpha = 1.0;
+  // TRSM kernel - caution with dtrsm
+  cblas_dtrsm(CblasRowMajor, CblasRight, CblasLower, CblasNoTrans, CblasNonUnit, M, N, alpha, L.data(), N, A.data(), N);
+  // return vector
+  return A;
+}
+
+// C = C - A * B
+std::vector<double> mkl_gemm_l_KK(std::vector<double> A,
+                                  std::vector<double> B,
+                                  std::vector<double> C,
+                                  std::size_t N,
+                                  std::size_t M)
+{
+  // GEMM constants
+  const double alpha = -1.0;
+  const double beta = 1.0;
+  // GEMM kernel - caution with dgemm
+  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+              M, N, N, alpha, B.data(), N, A.data(), N, beta, C.data(), N);
+  // return vector
+  return C;
+}
+
+// in-place solve X * L^T = A where L upper triangular
+std::vector<double> mkl_trsm_u_KK(std::vector<double> L,
+                                  std::vector<double> A,
+                                  std::size_t N,
+                                  std::size_t M)
+{
+  // TRSM constants
+  const double alpha = 1.0;
+  // TRSM kernel - caution with dtrsm
+  cblas_dtrsm(CblasRowMajor, CblasRight, CblasLower, CblasTrans, CblasNonUnit, M, N, alpha, L.data(), N, A.data(), N);
+  // return vector
+  return A;
+}
+
+// C = C - A^T * B
+std::vector<double> mkl_gemm_u_KK(std::vector<double> A,
+                                  std::vector<double> B,
+                                  std::vector<double> C,
+                                  std::size_t N,
+                                  std::size_t M)
+{
+  // GEMM constants
+  const double alpha = -1.0;
+  const double beta = 1.0;
+  // GEMM kernel - caution with dgemm
+  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans,
+              M, N, N, alpha, B.data(), N, A.data(), N, beta, C.data(), N);
+  // return vector
+  return C;
+}
