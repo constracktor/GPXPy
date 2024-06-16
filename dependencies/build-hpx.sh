5eeef7d87d5e6eae25aa30e9eb20f261653d4f15
MHelmann <maksim.helmann@gmail.com>
2024-06-16 10:56:17 +0200

    [build_hpx] add instrumentation=apex

--- dependencies/build-hpx.sh
+++ dependencies/build-hpx.sh
@@ -3,11 +3,11 @@
 set -ex
 # clone git repository
 git clone -c feature.manyFiles=true https://github.com/spack/spack.git
-# configure spack (add this to your .bashrc file)
+# configure spack (add the next line to your .bashrc file); purpose: automatically set up the Spack environment when you start a new shell session
 source spack/share/spack/setup-env.sh
 # find external software
 spack external find
 # install hpx and all its dependencies
-spack install hpx@1.9.1
+spack install hpx@1.9.1 instrumentation=apex
 # on pcsgs02-05 use
-# spack install hpx@1.9.1%gcc@9.4.0
+# spack install hpx@1.9.1%gcc@9.4.0 instrumentation=apex
