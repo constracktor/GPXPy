e7ca21990e971706befd9e83c8c574456126bdc7
MHelmann <maksim.helmann@gmail.com>
2024-06-19 00:22:44 +0200

    [gpppy_c.cpp] remove struct Hyperparameters; adjust constructor; implement optimize()

--- hpx_py11/cpp_code/src/gpppy_c.cpp
+++ hpx_py11/cpp_code/src/gpppy_c.cpp
@@ -1,6 +1,5 @@
 #include "../include/automobile_bits/gpppy_c.hpp"
 #include "../include/automobile_bits/utils_c.hpp"
-#include "../include/automobile_bits/gp_functions.hpp"
 
 #include <stdexcept>
 #include <iomanip>
@@ -30,21 +29,7 @@ namespace gpppy
         return oss.str();
     }
 
-    Hyperparameters::Hyperparameters(double lr, double b1, double b2, double eps, int opt_i)
-        : learning_rate(lr), beta1(b1), beta2(b2), epsilon(eps), opt_iter(opt_i) {}
-
-    std::string Hyperparameters::repr() const
-    {
-        std::ostringstream oss;
-        oss << "Hyperparameters: [learning_rate=" << learning_rate
-            << ", beta1=" << beta1
-            << ", beta2=" << beta2
-            << ", epsilon=" << epsilon
-            << ", opt_iter=" << opt_iter << "]";
-        return oss.str();
-    }
-
-    GP::GP(std::vector<double> input, std::vector<double> output, int n_tiles, int n_tile_size, double l, double v, double n, int n_r)
+    GP::GP(std::vector<double> input, std::vector<double> output, int n_tiles, int n_tile_size, double l, double v, double n, int n_r, std::vector<bool> trainable_bool)
     {
         _training_input = input;
         _training_output = output;
@@ -54,6 +39,7 @@ namespace gpppy
         vertical_lengthscale = v;
         noise_variance = n;
         n_regressors = n_r;
+        trainable_params = trainable_bool;
     }
 
     std::vector<double> GP::get_training_input() const
@@ -69,20 +55,24 @@ namespace gpppy
     std::string GP::repr() const
     {
         std::ostringstream oss;
-        oss << std::fixed << std::setprecision(5);
+        oss << std::fixed << std::setprecision(8);
         oss << "Kernel_Params: [lengthscale=" << lengthscale
             << ", vertical_lengthscale=" << vertical_lengthscale
             << ", noise_variance=" << noise_variance
-            << ", n_regressors=" << n_regressors << "]";
+            << ", n_regressors=" << n_regressors 
+            << ", trainable_params l=" << trainable_params[0]
+            << ", trainable_params v=" << trainable_params[1]
+            << ", trainable_params n=" << trainable_params[2]
+            << "]";
         return oss.str();
     }
 
     std::vector<std::vector<double>> GP::predict(const std::vector<double> &test_data, int m_tiles, int m_tile_size)
     {
-        hpx::shared_future<std::vector<std::vector<double>>> fut = preditc_hpx(_training_input, _training_output, test_data,
+        hpx::shared_future<std::vector<std::vector<double>>> fut = predict_hpx(_training_input, _training_output, test_data,
                                                                                _n_tiles, _n_tile_size, m_tiles, m_tile_size,
                                                                                lengthscale, vertical_lengthscale, noise_variance, n_regressors);
-    
+
         // hpx::async([input, output]()
         //  { return add_vectors(input, output); });
 
@@ -92,6 +82,26 @@ namespace gpppy
                                             result = fut.get(); // Wait for and get the result from the future
                                         });
         return result;
+    }
+
+    std::vector<double> GP::optimize(const gpppy_hyper::Hyperparameters &hyperparams)
+    {
+        hpx::shared_future<std::vector<double>> fut = optimize_hpx(_training_input, _training_output, _n_tiles, _n_tile_size,
+                                                                   lengthscale, vertical_lengthscale, noise_variance, n_regressors, 
+                                                                   hyperparams, trainable_params);
+
+        // hpx::async([input, output]()
+        //  { return add_vectors(input, output); });
+
+        std::vector<double> losses;
+        hpx::threads::run_as_hpx_thread([&losses, &fut]()
+                                        {
+                                            losses = fut.get(); // Wait for and get the result from the future
+                                        });
+
+        // Set new hyperparams
+
+        return losses;
     }
 
 }
