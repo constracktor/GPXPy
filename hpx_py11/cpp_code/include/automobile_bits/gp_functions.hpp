0b32b63848d77ff487364431e7f84944ec737832
MHelmann <maksim.helmann@gmail.com>
2024-06-19 00:12:03 +0200

    [gp_functions.hpp] add struct Hyperparameters and optimize_hpx()

--- hpx_py11/cpp_code/include/automobile_bits/gp_functions.hpp
+++ hpx_py11/cpp_code/include/automobile_bits/gp_functions.hpp
@@ -4,10 +4,31 @@
 
 #include <vector>
 #include <hpx/future.hpp>
-// generate a empty tile
-hpx::shared_future<std::vector<std::vector<double>>> preditc_hpx(const std::vector<double> &training_input, const std::vector<double> &training_output,
+
+namespace gpppy_hyper
+{
+    struct Hyperparameters
+    {
+        double learning_rate;
+        double beta1;
+        double beta2;
+        double epsilon;
+        int opt_iter;
+
+        Hyperparameters(double lr = 0.001, double b1 = 0.9, double b2 = 0.999, double eps = 1e-8, int opt_i = 0);
+
+        std::string repr() const;
+    };
+}
+
+hpx::shared_future<std::vector<std::vector<double>>> predict_hpx(const std::vector<double> &training_input, const std::vector<double> &training_output,
                                                                  const std::vector<double> &test_data, int n_tiles, int n_tile_size,
                                                                  int m_tiles, int m_tile_size, double lengthscale, double vertical_lengthscale,
                                                                  double noise_variance, int n_regressors);
+
+hpx::shared_future<std::vector<double>> optimize_hpx(const std::vector<double> &training_input, const std::vector<double> &training_output,
+                                                     int n_tiles, int n_tile_size, double &lengthscale, double &vertical_lengthscale,
+                                                     double &noise_variance, int n_regressors, const gpppy_hyper::Hyperparameters &hyperparams,
+                                                     std::vector<bool> trainable_params);
 
 #endif
