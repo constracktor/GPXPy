37c7132fdcee93859b198fda2f3e23a182f3f540
MHelmann <maksim.helmann@gmail.com>
2024-06-19 19:18:58 +0200

    [gp_functions] declare optimize_step_hpx; add M_T and V_T to Hyperparameters

--- hpx_py11/cpp_code/include/gp_headers/gp_functions.hpp
+++ hpx_py11/cpp_code/include/gp_headers/gp_functions.hpp
@@ -14,8 +14,12 @@ namespace gpppy_hyper
         double beta2;
         double epsilon;
         int opt_iter;
+        std::vector<double> M_T;
+        std::vector<double> V_T;
 
-        Hyperparameters(double lr = 0.001, double b1 = 0.9, double b2 = 0.999, double eps = 1e-8, int opt_i = 0);
+        Hyperparameters(double lr = 0.001, double b1 = 0.9,
+                        double b2 = 0.999, double eps = 1e-8, int opt_i = 0,
+                        std::vector<double> M_T = {0.0, 0.0, 0.0}, std::vector<double> V_T = {0.0, 0.0, 0.0});
 
         std::string repr() const;
     };
@@ -33,5 +37,10 @@ hpx::shared_future<std::vector<double>> optimize_hpx(const std::vector<double> &
                                                      int n_tiles, int n_tile_size, double &lengthscale, double &vertical_lengthscale,
                                                      double &noise_variance, int n_regressors, const gpppy_hyper::Hyperparameters &hyperparams,
                                                      std::vector<bool> trainable_params);
+
+hpx::shared_future<double> optimize_step_hpx(const std::vector<double> &training_input, const std::vector<double> &training_output,
+                                             int n_tiles, int n_tile_size, double &lengthscale, double &vertical_lengthscale,
+                                             double &noise_variance, int n_regressors, gpppy_hyper::Hyperparameters &hyperparams,
+                                             std::vector<bool> trainable_params, int iter);
 
 #endif
