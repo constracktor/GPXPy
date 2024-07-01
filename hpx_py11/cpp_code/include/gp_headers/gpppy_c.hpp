3ac20e510a761c01ec928e0c4de48dc809923595
MHelmann <maksim.helmann@gmail.com>
2024-07-01 11:11:39 +0200

    [gpppy_c.hpp] clean code; add comments

--- hpx_py11/cpp_code/include/gp_headers/gpppy_c.hpp
+++ hpx_py11/cpp_code/include/gp_headers/gpppy_c.hpp
@@ -14,12 +14,12 @@ namespace gpppy
         std::string file_path;
         std::vector<double> data;
 
+        // Initialize of the Gaussian process data constructor
         GP_data(const std::string &f_path, int n);
     };
 
     class GP
     {
-
     private:
         std::vector<double> _training_input;
         std::vector<double> _training_output;
@@ -33,21 +33,28 @@ namespace gpppy
         int n_regressors;
         std::vector<bool> trainable_params;
 
-        /// Constructor
+        // Initialize of the Gaussian process constructor
         GP(std::vector<double> input, std::vector<double> output, int n_tiles, int n_tile_size, double l, double v, double n, int n_r, std::vector<bool> trainable_bool);
 
+        // Print Gausian process attributes
         std::string repr() const;
 
+        // Return training input data
         std::vector<double> get_training_input() const;
 
+        // Return training output data
         std::vector<double> get_training_output() const;
 
+        // Predict output for test input and additionally provide uncertainty for the predictions
         std::vector<std::vector<double>> predict(const std::vector<double> &test_data, int m_tiles, int m_tile_size);
 
+        // Optimize hyperparameters for a specified number of iterations
         std::vector<double> optimize(const gpppy_hyper::Hyperparameters &hyperparams);
 
+        // Perform a single optimization step
         double optimize_step(gpppy_hyper::Hyperparameters &hyperparams, int iter);
 
+        // Calculate loss for given data and Gaussian process model
         double calculate_loss();
     };
 
