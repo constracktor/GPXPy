9f975b131ed16dae7a0575c4886a7358df09cccb
MHelmann <maksim.helmann@gmail.com>
2024-07-01 11:09:32 +0200

    [gp_functions.hpp] clean code; add comments

--- hpx_py11/cpp_code/include/gp_headers/gp_functions.hpp
+++ hpx_py11/cpp_code/include/gp_headers/gp_functions.hpp
@@ -1,6 +1,5 @@
 #ifndef GP_FUNCTIONS_H
 #define GP_FUNCTIONS_H
-// #define _USE_MATH_DEFINES
 
 #include <vector>
 #include <hpx/future.hpp>
@@ -17,27 +16,33 @@ namespace gpppy_hyper
         std::vector<double> M_T;
         std::vector<double> V_T;
 
+        // Initialize Hyperparameter constructor
         Hyperparameters(double lr = 0.001, double b1 = 0.9,
                         double b2 = 0.999, double eps = 1e-8, int opt_i = 0,
                         std::vector<double> M_T = {0.0, 0.0, 0.0}, std::vector<double> V_T = {0.0, 0.0, 0.0});
 
+        // Print Hyperparameter attributes
         std::string repr() const;
     };
 }
 
+// Compute the predictions and uncertainties
 hpx::shared_future<std::vector<std::vector<double>>> predict_hpx(const std::vector<double> &training_input, const std::vector<double> &training_output,
                                                                  const std::vector<double> &test_data, int n_tiles, int n_tile_size,
                                                                  int m_tiles, int m_tile_size, double lengthscale, double vertical_lengthscale,
                                                                  double noise_variance, int n_regressors);
 
+// Compute loss for given data and Gaussian process model
 hpx::shared_future<double> compute_loss_hpx(const std::vector<double> &training_input, const std::vector<double> &training_output,
                                             int n_tiles, int n_tile_size, int n_regressors, double *hyperparameters);
 
+// Perform optimization for a given number of iterations
 hpx::shared_future<std::vector<double>> optimize_hpx(const std::vector<double> &training_input, const std::vector<double> &training_output,
                                                      int n_tiles, int n_tile_size, double &lengthscale, double &vertical_lengthscale,
                                                      double &noise_variance, int n_regressors, const gpppy_hyper::Hyperparameters &hyperparams,
                                                      std::vector<bool> trainable_params);
 
+// Perform a single optimization step
 hpx::shared_future<double> optimize_step_hpx(const std::vector<double> &training_input, const std::vector<double> &training_output,
                                              int n_tiles, int n_tile_size, double &lengthscale, double &vertical_lengthscale,
                                              double &noise_variance, int n_regressors, gpppy_hyper::Hyperparameters &hyperparams,
