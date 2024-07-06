6a556c83728b5876a8ffae1453d81f770dc7c19b
MHelmann <maksim.helmann@gmail.com>
2024-07-06 17:42:07 +0200

    [gp_functions] declare predict_hpx and predict_with_uncertainty_hpx

--- hpx_py11/cpp_code/include/gp_headers/gp_functions.hpp
+++ hpx_py11/cpp_code/include/gp_headers/gp_functions.hpp
@@ -26,11 +26,17 @@ namespace gpppy_hyper
     };
 }
 
+// Compute the predictions
+hpx::shared_future<std::vector<double>> predict_hpx(const std::vector<double> &training_input, const std::vector<double> &training_output,
+                                                    const std::vector<double> &test_data, int n_tiles, int n_tile_size,
+                                                    int m_tiles, int m_tile_size, double lengthscale, double vertical_lengthscale,
+                                                    double noise_variance, int n_regressors);
+
 // Compute the predictions and uncertainties
-hpx::shared_future<std::vector<std::vector<double>>> predict_hpx(const std::vector<double> &training_input, const std::vector<double> &training_output,
-                                                                 const std::vector<double> &test_data, int n_tiles, int n_tile_size,
-                                                                 int m_tiles, int m_tile_size, double lengthscale, double vertical_lengthscale,
-                                                                 double noise_variance, int n_regressors);
+hpx::shared_future<std::vector<std::vector<double>>> predict_with_uncertainty_hpx(const std::vector<double> &training_input, const std::vector<double> &training_output,
+                                                                                  const std::vector<double> &test_data, int n_tiles, int n_tile_size,
+                                                                                  int m_tiles, int m_tile_size, double lengthscale, double vertical_lengthscale,
+                                                                                  double noise_variance, int n_regressors);
 
 // Compute loss for given data and Gaussian process model
 hpx::shared_future<double> compute_loss_hpx(const std::vector<double> &training_input, const std::vector<double> &training_output,
