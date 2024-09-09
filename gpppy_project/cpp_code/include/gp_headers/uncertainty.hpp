96a9a3bdf9a516a3c9ead5ae4f51a01b1619f992
MHelmann <maksim.helmann@gmail.com>
2024-09-09 15:12:26 +0200

    [uncertainty] remove mkl_trsm_l_KK, mkl_gemm_l_KK, mkl_gemm_cross_tcross_matrix

--- gpppy_project/cpp_code/include/gp_headers/uncertainty.hpp
+++ gpppy_project/cpp_code/include/gp_headers/uncertainty.hpp
@@ -71,23 +71,6 @@ std::vector<double> mkl_gemm_u_matrix(std::vector<double> A,
   return C;
 }
 
-// C = C - A^T * B
-std::vector<double> mkl_gemm_cross_tcross_matrix(std::vector<double> A,
-                                                 std::vector<double> B,
-                                                 std::vector<double> C,
-                                                 std::size_t N,
-                                                 std::size_t M)
-{
-  // GEMM constants
-  const double alpha = -1.0;
-  const double beta = 1.0;
-  // GEMM kernel - caution with dgemm
-  cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans,
-              M, M, N, alpha, A.data(), M, B.data(), M, beta, C.data(), M);
-  // return vector
-  return C;
-}
-
 // C = C - A * B
 std::vector<double> mkl_dot_uncertainty(std::vector<double> A,
                                         std::vector<double> R,
@@ -148,37 +131,6 @@ std::vector<double> diag_tile(const std::vector<double> &A,
   }
 
   return std::move(tile);
-}
-
-// in-place solve X * L = A where L lower triangular
-std::vector<double> mkl_trsm_l_KK(std::vector<double> L,
-                                  std::vector<double> A,
-                                  std::size_t N,
-                                  std::size_t M)
-{
-  // TRSM constants
-  const double alpha = 1.0;
-  // TRSM kernel - caution with dtrsm
-  cblas_dtrsm(CblasRowMajor, CblasLeft, CblasLower, CblasNoTrans, CblasNonUnit, N, M, alpha, L.data(), N, A.data(), M);
-  // return vector
-  return A;
-}
-
-// C = C - A * B
-std::vector<double> mkl_gemm_l_KK(std::vector<double> A,
-                                  std::vector<double> B,
-                                  std::vector<double> C,
-                                  std::size_t N,
-                                  std::size_t M)
-{
-  // GEMM constants
-  const double alpha = -1.0;
-  const double beta = 1.0;
-  // GEMM kernel - caution with dgemm
-  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-              N, M, N, alpha, A.data(), N, B.data(), M, beta, C.data(), M);
-  // return vector
-  return C;
 }
 
 // in-place solve X * L^T = A where L upper triangular
