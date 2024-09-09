80959f976eda17ce5cc39c4425b7c62019faeb88
MHelmann <maksim.helmann@gmail.com>
2024-09-09 15:09:28 +0200

    [mkl_adapter] move cblas functions from uncertainty.hpp to mkl_adapter

--- gpppy_project/cpp_code/include/gp_headers/mkl_adapter.hpp
+++ gpppy_project/cpp_code/include/gp_headers/mkl_adapter.hpp
@@ -166,4 +166,54 @@ std::vector<double> mkl_gemv_p(std::vector<double> A,
   return b;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// BLAS operations for uncertainty
+// in-place solve X * L = A where L lower triangular
+std::vector<double> mkl_trsm_l_KcK(std::vector<double> L,
+                                   std::vector<double> A,
+                                   std::size_t N,
+                                   std::size_t M)
+{
+  // TRSM constants
+  const double alpha = 1.0;
+  // TRSM kernel - caution with dtrsm
+  cblas_dtrsm(CblasRowMajor, CblasLeft, CblasLower, CblasNoTrans, CblasNonUnit, N, M, alpha, L.data(), N, A.data(), M);
+  // return vector
+  return A;
+}
+
+// C = C - A * B
+std::vector<double> mkl_gemm_l_KcK(std::vector<double> A,
+                                   std::vector<double> B,
+                                   std::vector<double> C,
+                                   std::size_t N,
+                                   std::size_t M)
+{
+  // GEMM constants
+  const double alpha = -1.0;
+  const double beta = 1.0;
+  // GEMM kernel - caution with dgemm
+  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+              N, M, N, alpha, A.data(), N, B.data(), M, beta, C.data(), M);
+  // return vector
+  return C;
+}
+
+// C = C - A^T * B
+std::vector<double> mkl_gemm_cross_tcross_matrix(std::vector<double> A,
+                                                 std::vector<double> B,
+                                                 std::vector<double> C,
+                                                 std::size_t N,
+                                                 std::size_t M)
+{
+  // GEMM constants
+  const double alpha = -1.0;
+  const double beta = 1.0;
+  // GEMM kernel - caution with dgemm
+  cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans,
+              M, M, N, alpha, A.data(), M, B.data(), M, beta, C.data(), M);
+  // return vector
+  return C;
+}
+
 #endif
