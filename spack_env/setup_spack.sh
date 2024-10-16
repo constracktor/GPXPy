671a096c3245fe4e4dee07f451811a96515c37aa
Henrik Möllmann <st178292@stud.uni-stuttgart.de>
2024-10-16 11:12:24 +0200

    Setup spack_env to install HPX with CUDA
    Changed gcc to 9.4.0 (required by CUDA)

--- spack_env/setup_spack.sh
+++ spack_env/setup_spack.sh
@@ -1,19 +1,28 @@
-# script to install spack and HPX on pcsgs02-05 (Version: 05.05.24)
 #!/usr/bin/env bash
+
+# script to install spack and HPX on pcsgs02-05 (Version: 14.10.24)
+
 set -ex
-# clone git repository
+
+# clone spack git repository into $HOME/spack
 DIR=$(pwd)
-cd
+cd $HOME
 git clone -c feature.manyFiles=true https://github.com/spack/spack.git
-# configure spack (add this to your .bashrc file)
+
+# configure spack (add this to your .bashrc file!)
 source $HOME/spack/share/spack/setup-env.sh
-# find external gcc
-module load gcc/13.2.0
-spack compiler find
-# find external software
+
+# load necessary modules on pcsgs02-05
+module load cuda/12.2.2
 module load cmake
+
+# find compilers and 
+spack compiler find
 spack external find
-# install HPX, MKL and all its dependencies
+spack external find cuda
+
+# install HPX, MKL, and its dependencies (specified in spack.yaml) into 
+# spack environment gpxpy (can be loaded with `spack env activate gpxpy`)
 spack env create gpxpy
 cp $DIR/spack_env/spack.yaml $HOME/spack/var/spack/environments/gpxpy/spack.yaml
 spack env activate gpxpy
