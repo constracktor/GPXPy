d85678a0c7d2c09e4f5aebcabda87db74ca43123
Henrik Möllmann <henmoell@gmail.com>
2024-11-17 23:54:53 +0100

    Fixed spack_env to use similar setup to spack_env_simcl1

--- spack_env/setup_spack.sh
+++ spack_env/setup_spack.sh
@@ -8,16 +8,29 @@
 # - may require manual changes depending on your system
 ################################################################################
 
-# exit on error; print each command
+# exit on error (non-zero status); print each command before execution
 set -ex
 
-# clone Spack repo in $HOME/spack
-DIR=$(pwd)
-cd $HOME
-git clone -c feature.manyFiles=true https://github.com/spack/spack.git
+# Install Spack ------------------------------------------------------------ {{{
+
+# Clone spack git repository into $HOME/spack
+
+# check if current directory is spack_env
+if [[ $(basename "$(pwd)") != "spack_env" ]]; then
+    echo "Please run this script from the spack_envdirectory"
+    exit 1
+fi
+git clone -c feature.manyFiles=true https://github.com/spack/spack.git ~/spack
+
+# use custom HPX package.py: added some CMake options at the end
+cp ./hpx_package.py $HOME/spack/var/spack/repos/builtin/packages/hpx/package.py
 
 # setup `spack` command
 source $HOME/spack/share/spack/setup-env.sh
+
+# }}} --------------------------------------------------------------------------
+
+# Install HPX -------------------------------------------------------------- {{{
 
 # intentionally not locating system-local packages, against problems with
 # locally installed packages
@@ -25,7 +38,10 @@ source $HOME/spack/share/spack/setup-env.sh
 # install dependencies into `gpxpy` spack environment
 # (see spack.yaml in same directory for details)
 spack env create gpxpy
-cp $DIR/spack_env/spack.yaml $HOME/spack/var/spack/environments/gpxpy/spack.yaml
+cp ./spack.yaml $HOME/spack/var/spack/environments/gpxpy/spack.yaml
 spack env activate gpxpy
 spack concretize -f
+mkdir -p $HOME/spack/var/spack/environments/gpxpy/.spack-env/view/include
 spack install
+
+# }}} --------------------------------------------------------------------------
