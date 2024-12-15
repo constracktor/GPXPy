f88c6ce6fa070cd9a9a4cd0af9f2d3e1dc04a682
Tim Niederhausen <st144339@stud.uni-stuttgart.de>
2024-12-15 21:06:17 +0100

    chore: Add better dev presets and document them

--- compile_gpxpy_cpp.sh
+++ compile_gpxpy_cpp.sh
@@ -9,18 +9,16 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 # Load GCC compiler
 module load gcc/13.2.0
 module load cmake
-CC_COMPILER=gcc
-CXX_COMPILER=g++
+export CC=gcc
+export CXX=g++
 # Activate spack environment
 spack env activate gpxpy_cpu_gcc
 # # Load Clang compiler
 # module load clang/17.0.1
-# CC_COMPILER=clang
-# CXX_COMPILER=clang++
+# export CC=clang
+# export CXX=clang++
 # # Activate spack environment
 # spack env activate gpxpy_gpu_clang
-# Set cmake command
-export CMAKE_COMMAND=$(which cmake)
 # Configure APEX
 export APEX_SCREEN_OUTPUT=1
 # Configure MKL
@@ -29,13 +27,7 @@ export MKL_CONFIG='-DMKL_ARCH=intel64 -DMKL_LINK=dynamic -DMKL_INTERFACE_FULL=in
 ################################################################################
 # Compile code
 ################################################################################
-rm -rf build_cpp && mkdir build_cpp && cd build_cpp
-# Configure the project
-$CMAKE_COMMAND ../core -DCMAKE_BUILD_TYPE=Release \
-                  -DHPX_WITH_DYNAMIC_HPX_MAIN=ON \
-                  -DCMAKE_C_COMPILER=$(which $CC_COMPILER) \
-		  -DCMAKE_CXX_COMPILER=$(which $CXX_COMPILER) \
-                  ${MKL_CONFIG}
- # Build the project
-make -j all
-make install
+# note: dev defaults to debug builds!
+cmake --preset dev-linux -DGPXPY_BUILD_BINDINGS=OFF
+cmake --build --preset dev-linux
+# ctest --preset dev-linux
--- compile_gpxpy_python.sh
+++ compile_gpxpy_python.sh
@@ -9,18 +9,16 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 # Load GCC compiler
 module load gcc/13.2.0
 module load cmake
-CC_COMPILER=gcc
-CXX_COMPILER=g++
+export CC=gcc
+export CXX=g++
 # Activate spack environment
 spack env activate gpxpy_cpu_gcc
 # # Load Clang compiler
 # module load clang/17.0.1
-# CC_COMPILER=clang
-# CXX_COMPILER=clang++
+# export CC=clang
+# export CXX=clang++
 # # Activate spack environment
 # spack env activate gpxpy_gpu_clang
-# Set cmake command
-export CMAKE_COMMAND=$(which cmake)
 # Configure APEX
 export APEX_SCREEN_OUTPUT=1
 # Configure MKL
@@ -29,14 +27,8 @@ export MKL_CONFIG='-DMKL_ARCH=intel64 -DMKL_LINK=dynamic -DMKL_INTERFACE_FULL=in
 ################################################################################
 # Compile code
 ################################################################################
-rm -rf build_python && mkdir build_python && cd build_python
-# Configure the project
-$CMAKE_COMMAND .. -DCMAKE_BUILD_TYPE=Release \
-                  -DPYTHON_LIBRARY_DIR=$(python3 -c "import site; print(site.getsitepackages()[0])") \
-                  -DPYTHON_EXECUTABLE=$(which python3) \
-                  -DHPX_WITH_DYNAMIC_HPX_MAIN=ON \
-                  -DCMAKE_C_COMPILER=$(which $CC_COMPILER) \
-		  -DCMAKE_CXX_COMPILER=$(which $CXX_COMPILER) \
-                  ${MKL_CONFIG}
- # Build the project
-make -j all
+# note: dev defaults to debug builds!
+cmake --preset dev-linux
+cmake --build --preset dev-linux
+# ctest --preset dev-linux
+
