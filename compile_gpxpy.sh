9b67d37989f30986331e7059965db29be1aec0eb
constracktor <strack.as@icloud.com>
2025-01-02 00:43:57 +0100

    Unify compilation into one script

--- compile_gpxpy.sh
+++ compile_gpxpy.sh
@@ -8,26 +8,36 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 ################################################################################
 # Load GCC compiler
 module load gcc/13.2.0
-module load cmake
 export CC=gcc
 export CXX=g++
 # Activate spack environment
 spack env activate gpxpy_cpu_gcc
+
 # # Load Clang compiler
 # module load clang/17.0.1
 # export CC=clang
 # export CXX=clang++
 # # Activate spack environment
 # spack env activate gpxpy_gpu_clang
+
 # Configure APEX
-export APEX_SCREEN_OUTPUT=1
-# Configure MKL
-export MKL_CONFIG='-DMKL_ARCH=intel64 -DMKL_LINK=dynamic -DMKL_INTERFACE_FULL=intel_lp64 -DMKL_THREADING=sequential'
+#export APEX_SCREEN_OUTPUT=1
+
+# Bindings
+export BINDINGS=ON
+export INSTALL_DIR=$(pwd)/examples/gpxpy_python
+# export BINDINGS=OFF
+# export INSTALL_DIR=$(pwd)/examples/gpxpy_cpp
+
+# Release:	ci-ubuntu
+# Debug:	dev-linux
+export PRESET=ci-ubuntu
 
 ################################################################################
 # Compile code
 ################################################################################
 # note: dev defaults to debug builds!
-cmake --preset dev-linux -DGPXPY_BUILD_BINDINGS=OFF
-cmake --build --preset dev-linux
-# ctest --preset dev-linux
+cmake --preset $PRESET -DGPXPY_BUILD_BINDINGS=$BINDINGS -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+cmake --build --preset $PRESET
+cmake --install build/$PRESET
+# ctest --preset $PRESET
